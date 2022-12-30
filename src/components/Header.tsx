eff7330897e032fdd2a4526705672851f6df043c
EngCode <elampratorabdallah14@gmail.com>
2022-12-30 07:05:55 +0200

    update.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header className="bg-white pdy-10 pdx-30 container-fluid align-center-y align-between position-st pos-top-0 z-index-header">
             {/*===> Logo <===*/}
             <a href="/" className="logo">
-                <img src="/logo-rtl.png" alt="Mitanok Editor" className="hidden-sm-down" width="121" height="36" />
+                <img src="/pds-logo.svg" alt="Mitanok Editor" className="hidden-sm-down" width="121" height="36" />
                 <img src="/fevicon.png" alt="Mitanok Editor" className="hidden-md-up" width="36" height="36" />
             </a>
             {/*===> Buttons <===*/}
